3359b40fdc84e7bb903fb926f467e936071b06d4
leonidlykov <lykov3123@gmail.com>
2023-09-07 16:04:00 +0300

    sm2

---
+++ js/main.js
@@ -0,0 +1,8 @@
+let city = prompt ('Введите название города');
+let years = prompt('Введите год образования города');
+let people = prompt ('Введите население города');
+let y = 2023;
+let x = years;
+let years_old =(y-x);
+alert ('Городу '+city+ ' исполнилось ' +years_old+ ' лет с момента образования. Население -'+people+ ' человек');
+
